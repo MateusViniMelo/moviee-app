7406d44bd2d6f46e57e011f4a3f585c668b1cb96
Mateus Melo <mateusmelo04@gmail.com>
2023-11-09 18:14:21 -0300

    [feature] adição do scroll infinito

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,58 @@
 <template>
-    <div>
-        <h1 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 uppercase dark:text-white">Filmes bem avaliados</h1>
+  <div>
+    <h1
+      class="mb-3 text-2xl font-bold tracking-tight text-gray-900 uppercase dark:text-white"
+    >
+      Filmes bem avaliados
+    </h1>
 
-        <swiper :slidesPerView="slidesPorView" :freeMode="true" :navigation="true" :spaceBetween="20" :pagination="{
-            clickable: true,
-        }" :modules="[Navigation, FreeMode]" @resize="setSlidesPorTamanho()">
-            <swiper-slide v-for=" filme  in  filmeResponse?.results " :key="filme.id">
-                <img class="rounded-t-lg" :src="`${config.public.imageUrl}${filme?.poster_path}`"
-                    :alt="`${filme?.title}`" />
-
-            </swiper-slide>
-
-        </swiper>
-
-    </div>
+    <swiper
+      :slidesPerView="slidesPorView"
+      :loop="true"
+      :freeMode="true"
+      :navigation="true"
+      :spaceBetween="20"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="[Navigation, FreeMode]"
+      @resize="setSlidesPorTamanho()"
+    >
+      <swiper-slide v-for="filme in filmeResponse?.results" :key="filme.id">
+        <img
+          class="rounded-t-lg"
+          :src="`${config.public.imageUrl}${filme?.poster_path}`"
+          :alt="`${filme?.title}`"
+        />
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { FreeMode, Navigation } from "swiper/modules";
 
-import { FreeMode, Navigation  } from 'swiper/modules';
+import "swiper/css";
 
-import 'swiper/css';
-
-import 'swiper/css/pagination';
-
+import "swiper/css/pagination";
 
 const isClient = process.client;
-const filmeStore = useFilmeStore()
-const tamanhoSliderStore = useTamanhoSliderStore()
+const filmeStore = useFilmeStore();
+const tamanhoSliderStore = useTamanhoSliderStore();
 const config = useRuntimeConfig();
-const { getGenresMovies } = useGenreMovie()
+const { getGenresMovies } = useGenreMovie();
 
-const { filmeResponse } = storeToRefs(filmeStore)
-const { slidesPorView } = storeToRefs(tamanhoSliderStore)
+const { filmeResponse } = storeToRefs(filmeStore);
+const { slidesPorView } = storeToRefs(tamanhoSliderStore);
 
-const { loadFilmesBemAvaliados } = filmeStore
-const { setSlidesPorTamanho } = tamanhoSliderStore
-
-
-
-
+const { loadFilmesBemAvaliados } = filmeStore;
+const { setSlidesPorTamanho } = tamanhoSliderStore;
 
 onMounted(async () => {
-    setSlidesPorTamanho()
+  setSlidesPorTamanho();
 
-    loadFilmesBemAvaliados()
-
-})
+  loadFilmesBemAvaliados();
+});
 </script>
 
 <style scoped></style>
-
