0e7c7f7f1f0dc05ca2aa850b16fba3fb7489121e
Mateus Melo <mateusmelo04@gmail.com>
2023-11-14 11:13:33 -0300

    [feature] novo banner dos filmes populares

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <Navbar />
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900 ">
-            <div class="container px-4 pt-16 pb-12 mx-auto mt-16">
+            <div class="pt-2 pb-12 mx-auto mt-16 ">
                 <slot></slot>
             </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,118 +1,129 @@
 <template>
   <div>
     <swiper
-      :slidesPerView="slidesPorViewPopular"
+      :pagination="{
+        clickable: true,
+      }"
       :loop="true"
-      :freeMode="true"
       :navigation="true"
       :spaceBetween="20"
       :autoplay="{
         delay: 2500,
         disableOnInteraction: false,
       }"
-      :pagination="{
-        clickable: true,
-      }"
-      :modules="[Navigation, FreeMode, Autoplay]"
+      :modules="[Navigation, Autoplay, Pagination]"
       @resize="setSlidesPorTamanhoFilmePopular()"
-    >
-      <swiper-slide v-for="filme in filmesPopulares?.results" :key="filme.id">
-        <img
-          class="rounded-lg"
-          :src="`${config.public.imageUrl}/w500${filme?.poster_path}`"
-          :alt="`${filme?.title}`"
-        />
-      </swiper-slide>
-    </swiper>
-
-    <h1
-      class="mt-40 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Filmes bem avaliados
-    </h1>
-
-    <swiper
-      :slidesPerView="slidesPorView"
-      :loop="true"
-      :freeMode="true"
-      :navigation="true"
-      :spaceBetween="20"
-      :pagination="{
-        clickable: true,
-      }"
-      :modules="[Navigation, FreeMode]"
-      @resize="setSlidesPorTamanho()"
-      class=""
+      class="w-full lg:h-[600px] p-24 h-full"
     >
       <swiper-slide
-        v-for="filme in filmesBemAvaliados?.results"
+        v-for="filme in filmesPopulares?.results"
         :key="filme.id"
-         class=""
+        class="relative"
       >
         <img
-          class="rounded-t-lg "
-          :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
+          class="w-full m-auto"
+          :src="`${config.public.imageUrl}/original${filme?.backdrop_path}`"
           :alt="`${filme?.title}`"
         />
+        <div
+          class="absolute bottom-0 flex flex-col items-start justify-end w-full p-10 space-y-5 text-lg text-white lg:p-20 h-2/3 degrade-preto"
+          style=""
+        >
+          <p class="font-bold shadow lg:w-1/2 xl:text-6xl md:text-4xl">{{ filme?.title }}</p>
+          <p class="hidden text-sm text-left lg:text-base text-clip xl:w-1/2 md:block">{{ filme?.overview }}</p>
+        </div>
       </swiper-slide>
     </swiper>
-    <h1
-      class="mt-20 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Novidades
-    </h1>
 
-    <swiper
-      :slidesPerView="slidesPorView"
-      :loop="true"
-      :freeMode="true"
-      :navigation="true"
-      :spaceBetween="20"
-      :pagination="{
-        clickable: true,
-      }"
-      :modules="[Navigation, FreeMode]"
-      @resize="setSlidesPorTamanho()"
-    >
-      <swiper-slide
-        v-for="filme in filmesLancadosEmBreve?.results"
-        :key="filme.id"
+    <div class="container px-4 mx-auto">
+      <h1
+        class="mt-10 mb-3 text-2xl font-bold tracking-tight text-gray-900 lg:mt-20 dark:text-white"
       >
-        <img
-          class="rounded-t-lg"
-          :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
-          :alt="`${filme?.title}`"
-        />
-      </swiper-slide>
-    </swiper>
+        Filmes bem avaliados
+      </h1>
 
-    <h1
-      class="mt-20 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      Filmes em Cartaz
-    </h1>
+      <swiper
+        :slidesPerView="slidesPorView"
+        :loop="true"
+        :freeMode="true"
+        :navigation="true"
+        :spaceBetween="20"
+        :pagination="{
+          clickable: true,
+        }"
+        :modules="[Navigation, FreeMode]"
+        @resize="setSlidesPorTamanho()"
+        class=""
+      >
+        <swiper-slide
+          v-for="filme in filmesBemAvaliados?.results"
+          :key="filme.id"
+          class=""
+        >
+          <img
+            class="rounded-t-lg"
+            :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
+            :alt="`${filme?.title}`"
+          />
+        </swiper-slide>
+      </swiper>
+      <h1
+        class="mt-20 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Novidades
+      </h1>
 
-    <swiper
-      :slidesPerView="slidesPorView"
-      :loop="true"
-      :freeMode="true"
-      :navigation="true"
-      :spaceBetween="20"
-      :pagination="{
-        clickable: true,
-      }"
-      :modules="[Navigation, FreeMode]"
-      @resize="setSlidesPorTamanho()"
-      
-    >
-      <swiper-slide v-for="filme in filmesEmCartaz?.results" :key="filme.id">
-        <img
-          class="rounded-t-lg"
-          :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
-          :alt="`${filme?.title}`"
-        />
-      </swiper-slide>
-    </swiper>
+      <swiper
+        :slidesPerView="slidesPorView"
+        :loop="true"
+        :freeMode="true"
+        :navigation="true"
+        :spaceBetween="20"
+        :pagination="{
+          clickable: true,
+        }"
+        :modules="[Navigation, FreeMode]"
+        @resize="setSlidesPorTamanho()"
+      >
+        <swiper-slide
+          v-for="filme in filmesLancadosEmBreve?.results"
+          :key="filme.id"
+        >
+          <img
+            class="rounded-t-lg"
+            :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
+            :alt="`${filme?.title}`"
+          />
+        </swiper-slide>
+      </swiper>
+
+      <h1
+        class="mt-20 mb-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        Filmes em Cartaz
+      </h1>
+
+      <swiper
+        :slidesPerView="slidesPorView"
+        :loop="true"
+        :freeMode="true"
+        :navigation="true"
+        :spaceBetween="20"
+        :pagination="{
+          clickable: true,
+        }"
+        :modules="[Navigation, FreeMode]"
+        @resize="setSlidesPorTamanho()"
+      >
+        <swiper-slide v-for="filme in filmesEmCartaz?.results" :key="filme.id">
+          <img
+            class="rounded-t-lg"
+            :src="`${config.public.imageUrl}/w200${filme?.poster_path}`"
+            :alt="`${filme?.title}`"
+          />
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
@@ -160,4 +171,8 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.degrade-preto {
+  background-image: linear-gradient(rgb(0, 0, 0, 0), rgb(0, 0, 0, 1));
+}
+</style>
