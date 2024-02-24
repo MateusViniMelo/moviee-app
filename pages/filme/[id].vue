5e38008933da3e9beafa674c4a54ce1c25092a1d
Mateus Melo <mateusmelo04@gmail.com>
2024-02-23 22:21:19 -0300

    [feature] adição de recomendados e mudança de UI

--- pages/filme/[id].vue
+++ pages/filme/[id].vue
@@ -1,24 +1,22 @@
 <template>
   <div
-    class="relative h-full bg-center bg-no-repeat bg-cover"
+    class="relative bg-fixed bg-top bg-no-repeat bg-cover"
     :style="`background-image: url( '${config.public.imageUrl}/original${filmeInformacoes?.filme?.backdrop_path}');`"
   >
     <div class="relative z-10">
       <div class="container h-full px-4 mx-auto">
         <div class="text-lg text-gray-900 dark:text-white"></div>
-        <div
-          class="grid grid-cols-2 pt-16 xl:grid-cols-4 md:pt-48 md:grid-cols-6"
-        >
+        <div class="grid grid-cols-2 pt-16 xl:pt-72 md:grid-cols-6 md:pt-52">
           <div
-            class="flex items-start justify-center col-span-2 xl:col-span-1 md:col-span-3 xl:justify-normal lg:col-span-3"
+            class="flex items-start justify-center col-span-2 xl:col-span-2 md:col-span-3 xl:justify-center lg:col-span-3"
           >
             <img
               :src="`${config.public.imageUrl}/original${filmeInformacoes?.filme?.poster_path}`"
               alt=""
-              class="rounded-lg w-[300px] h-auto"
+              class="w-10/12 h-auto rounded-lg"
             />
           </div>
-          <div class="col-span-2 xl:col-span-3 md:col-span-3 lg:col-span-3">
+          <div class="col-span-2 xl:col-span-4 md:col-span-3 lg:col-span-3">
             <div>
               <h5
                 class="mt-3 mb-5 text-4xl font-bold tracking-tight text-center text-gray-900 md:text-5xl dark:text-white md:text-left"
@@ -53,7 +51,7 @@
               >
                 <NuxtLink
                   :to="`/filmes-genre/${genero.id}`"
-                  target="_blank"
+                  
                   v-for="genero in filmeInformacoes?.filme?.genres"
                   :key="genero.id"
                   class="font-medium me-2 px-2.5 py-0.5 rounded bg-yellow-300 text-dark focus:outline-none hover:bg-yellow-400 focus:ring-yellow-300 dark:focus:ring-yellow-900"
@@ -73,7 +71,9 @@
               <h5
                 class="mt-6 mb-5 text-2xl font-bold text-gray-900 uppercase dark:text-white"
               >
-                <span class="pr-3 border-b-4 border-yellow-300 pb-0.5">Elenco</span>
+                <span class="pr-3 border-b-4 border-yellow-300 pb-0.5"
+                  >Elenco</span
+                >
               </h5>
               <div>
                 <swiper
@@ -118,46 +118,56 @@
                 </swiper>
               </div>
             </div>
+            <div class="mt-32">
+              <h5
+                class="mt-6 mb-5 text-2xl font-bold text-gray-900 uppercase dark:text-white"
+              >
+                <span class="pr-3 border-b-4 border-yellow-300 pb-0.5"
+                  >Imagens</span
+                >
+              </h5>
+              <div class="flex justify-end">
+                <swiper
+                  :slidesPerView="1"
+                  :spaceBetween="30"
+                  :loop="true"
+                  :autoplay="{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                  }"
+                  :pagination="{
+                    dynamicBullets: true,
+                  }"
+                  :navigation="true"
+                  :modules="[Pagination, Navigation, Autoplay]"
+                  class=""
+                >
+                  <swiper-slide
+                    v-for="(imagem, index) in filmeInformacoes?.movieImagens
+                      ?.backdrops"
+                    :key="index"
+                  >
+                    <img
+                      class="rounded-lg"
+                      :src="`${config.public.imageUrl}/original${imagem?.file_path}`"
+                      alt="image description"
+                    />
+                  </swiper-slide>
+                </swiper>
+              </div>
+            </div>
           </div>
 
-          <div></div>
+          
         </div>
-        <div class="mt-32">
-          <div class="flex justify-start">
-            <swiper
-              :slidesPerView="1"
-              :spaceBetween="30"
-              :loop="true"
-              :autoplay="{
-                delay: 2500,
-                disableOnInteraction: false,
-              }"
-              :pagination="{
-                dynamicBullets: true,
-              }"
-              :navigation="true"
-              :modules="[Pagination, Navigation, Autoplay]"
-              class="w-2/3"
-            >
-              <swiper-slide
-                v-for="(imagem, index) in filmeInformacoes?.movieImagens
-                  ?.backdrops"
-                :key="index"
-              >
-                <img
-                  class="rounded-lg"
-                  :src="`${config.public.imageUrl}/original${imagem?.file_path}`"
-                  alt="image description"
-                />
-              </swiper-slide>
-            </swiper>
-          </div>
-        </div>
+
         <div class="">
           <h1
             class="mt-20 mb-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
           >
-            <span class="pr-3 border-b-4 border-yellow-300 pb-0.5">Recomendações</span>
+            <span class="pr-3 border-b-4 border-yellow-300 pb-0.5"
+              >Recomendações</span
+            >
           </h1>
 
           <swiper
@@ -188,25 +198,21 @@
       </div>
     </div>
     <div
-      class="absolute top-0 left-0 z-0 w-full h-full bg-gradient-to-t from-blue-50 from-70% to-transparent dark:from-gray-900"
+      class="absolute top-0 left-0 z-0 w-full h-full bg-gradient-to-t from-blue-50 from-75% to-transparent dark:from-gray-900"
     ></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { FilmeInformacao } from "~/types/filmeInformacao";
 import { FreeMode, Navigation, Pagination, Autoplay } from "swiper/modules";
 import { initFlowbite } from "flowbite";
 import "swiper/css";
 import "swiper/css/pagination";
-import type { PessoaElenco } from "~/types/pessoaElenco";
-import type { CreditoFilme } from "~/types/creditoFilme";
 const { $http } = useNuxtApp();
 const tamanhoSliderStore = useTamanhoSliderStore();
 const { slidesElencoPorView, slidesPorView } = storeToRefs(tamanhoSliderStore);
 const { setSlidesElencoPorTamanho, setSlidesPorTamanho } = tamanhoSliderStore;
 const route = useRoute();
-const router = useRouter();
 const config = useRuntimeConfig();
 
 const { data: filmeInformacoes } = await useAsyncData("movie", async () => {
@@ -225,6 +231,7 @@ const { data: filmeInformacoes } = await useAsyncData("movie", async () => {
 onMounted(() => {
   setSlidesElencoPorTamanho();
   setSlidesPorTamanho();
+  initFlowbite();
 });
 </script>
 
