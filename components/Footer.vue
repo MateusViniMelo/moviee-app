6be3cadbc5832e9e54e9e40f86b3e8d41a056864
Mateus Melo <mateusmelo04@gmail.com>
2024-06-18 09:00:00 -0300

    [feature] inclusão de um novo footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,93 @@
 <template>
-  <div>
-    <footer class="bg-white shadow dark:bg-gray-800">
-      <div
-        class="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between"
-      >
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-          >© 2023
-          <a  class="hover:underline">Mateus V.S Melo</a
-          >. All Rights Reserved.
-        </span>
-        <ul
-          class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
-        >
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Contact</a>
-          </li>
-        </ul>
+  <footer class="bg-gray-900 border-t border-gray-700 ">
+    <div class="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
+      <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+          <p class="flex items-center">
+            <a href="https://meu-portifolio-nuxt.vercel.app/" target="_blank"
+              class="self-center text-2xl font-semibold text-white whitespace-nowrap hover:text-gray-200"
+              >Mateus<span class="text-violet-600">;</span></a
+            >
+          </p>
+        </div>
+        <div class="grid grid-cols-2 gap-8 space-x-4 sm:gap-12">
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-white uppercase">
+              Tecnologias
+            </h2>
+            <ul class="font-medium text-gray-400">
+              <li class="mb-4">
+                <a
+                  href="https://nuxt.com/"
+                  class="hover:underline"
+                  target="_blank"
+                  >Nuxt 3</a
+                >
+              </li>
+              <li>
+                <a href="https://tailwindcss.com/" class="hover:underline"
+                  >Tailwind CSS</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-white uppercase">
+              Me siga
+            </h2>
+            <ul class="font-medium text-gray-400">
+              <li class="mb-4">
+                <a
+                  href="https://github.com/MateusViniMelo"
+                  target="_blank"
+                  class="hover:underline"
+                  >Github</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/mateus-melo-55bb55193/"
+                  target="_blank"
+                  class="hover:underline"
+                  >Linkedin</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </footer>
-  </div>
+      <hr class="my-6 border-gray-700 sm:mx-auto lg:my-8" />
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <span class="text-sm text-gray-400 sm:text-center"
+          >© 2024 - {{ new Date().getFullYear() }}
+          <span class="hover:underline">Mateus</span>. All Rights
+          Reserved.</span
+        >
+        <div class="flex mt-4 sm:justify-center sm:mt-0">
+          <a
+            href="https://github.com/MateusViniMelo"
+            target="_blank"
+            class="text-gray-500 hover:text-white ms-5"
+          >
+            <Icon name="mdi:github" :ssr="true" size="18" />
+
+            <span class="sr-only">Conta GitHub</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mateus-melo-55bb55193/"
+            target="_blank"
+            class="text-gray-500 hover:text-white ms-5"
+          >
+            <Icon name="mdi:linkedin" :ssr="true" size="18" />
+
+            <span class="sr-only">Conta Linkedin</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         
         <Navbar />
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900 ">
+        <div class="min-h-screen pb-32 bg-gray-100 dark:bg-gray-900">
             <div class="pt-2 pb-12 mx-auto mt-16 ">
                 <slot></slot>
                 
